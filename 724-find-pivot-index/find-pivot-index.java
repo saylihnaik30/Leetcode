58fd73a7e22e9a5a748e14273002df56bd4eebf9
Sayli Naik <saylihnaik30@gmail.com>
2026-08-22 11:11:28 +0530

    Time: 1 ms (97.51%) | Memory: 47.1 MB (93.98%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n= nums.length;
+        int total = 0;
+        for(int i=0; i<n; i++){
+            total += nums[i];
+        }
+        int left = 0;
+        for(int i=0; i<n; i++){
+            int right = total-left - nums[i];
+            if(left==right){
+                return i;
+            }
+            else {
+                left= left+nums[i];
+            }
+        }
+        return -1;
+    }
+}
