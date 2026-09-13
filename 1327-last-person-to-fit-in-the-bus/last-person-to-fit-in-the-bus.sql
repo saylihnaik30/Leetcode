3cd9eca633a661d910d5bba39fed196895e3a268
Sayli Naik <saylihnaik30@gmail.com>
2026-09-13 11:22:41 +0530

    Time: 766 ms (93.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,12 @@
+WITH running_total AS (
+    SELECT
+        person_name,
+        turn,
+        SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+)
+SELECT person_name
+FROM running_total
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
