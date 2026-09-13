78e69ed1030e228b65051c2678eef51cae92a995
Sayli Naik <saylihnaik30@gmail.com>
2026-09-13 11:10:17 +0530

    Time: 792 ms (14.50%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs AS l1
+JOIN Logs AS l2
+    ON l2.id = l1.id + 1
+JOIN Logs AS l3
+    ON l3.id = l2.id + 1
+WHERE l1.num = l2.num
+  AND l2.num = l3.num;
